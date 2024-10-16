4e014a240c94ea869647c1df5f5ff0f6efaec2a4
imane <imanezenasnii@gmail.com>
2024-10-16 21:27:46 +0100

    Apply corrections

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,74 +1,38 @@
-import readDatabase from '../utils';
+import { readDatabase } from '../utils.js';
 
-/**
- * The list of supported majors.
- */
-const VALID_MAJORS = ['CS', 'SWE'];
-
-/**
- * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 class StudentsController {
-  static getAllStudents(request, response) {
-    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    static async getAllStudents(req, res) {
+        try {
+            const path = process.argv.length > 2 ? process.argv[2] : './database.csv';
 
-    readDatabase(dataPath)
-      .then((studentGroups) => {
-        const responseParts = ['This is the list of our students'];
-        // A comparison function for ordering a list of strings in ascending
-        // order by alphabetic order and case insensitive
-        const cmpFxn = (a, b) => {
-          if (a[0].toLowerCase() < b[0].toLowerCase()) {
-            return -1;
-          }
-          if (a[0].toLowerCase() > b[0].toLowerCase()) {
-            return 1;
-          }
-          return 0;
-        };
-
-        for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
-          responseParts.push([
-            `Number of students in ${field}: ${group.length}.`,
-            'List:',
-            group.map((student) => student.firstname).join(', '),
-          ].join(' '));
+            const students = await readDatabase(path);
+            const fields = Object.keys(students).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+            
+            let response = 'This is the list of our students\n';
+            for (const field of fields) {
+                response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+            }
+            res.status(200).send(response);
+        } catch (error) {
+            res.status(500).send('Cannot load the database');
         }
-        response.status(200).send(responseParts.join('\n'));
-      })
-      .catch((err) => {
-        response
-          .status(500)
-          .send(err instanceof Error ? err.message : err.toString());
-      });
-  }
-
-  static getAllStudentsByMajor(request, response) {
-    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
-    const { major } = request.params;
-
-    if (!VALID_MAJORS.includes(major)) {
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
     }
-    readDatabase(dataPath)
-      .then((studentGroups) => {
-        let responseText = '';
 
-        if (Object.keys(studentGroups).includes(major)) {
-          const group = studentGroups[major];
-          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
+    static async getAllStudentsByMajor(req, res) {
+        const { major } = req.params;
+        if (major !== 'CS' && major !== 'SWE') {
+            return res.status(500).send('Major parameter must be CS or SWE');
         }
-        response.status(200).send(responseText);
-      })
-      .catch((err) => {
-        response
-          .status(500)
-          .send(err instanceof Error ? err.message : err.toString());
-      });
-  }
+        try {
+            const students = await readDatabase('./database.csv');
+            const response = `List: ${students[major] ? students[major].join(', ') : ''}`;
+            // const response = `List: ${students.map((student) => student.firstname).join(', ')}`;
+            res.status(200).send(response);
+        } catch (error) {
+            res.status(500).send('Cannot load the database');
+        //   .send(err instanceof Error ? err.message : err.toString());
+        }
+    }
 }
 
 export default StudentsController;
-module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,14 +1,15 @@
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+// app.use('/app', router); // All routes in 'router' will be prefixed with '/app'
+import AppController from '../controllers/AppController.js';
+import StudentsController from '../controllers/StudentsController.js';
+import express from 'express'
 
 /**
  * Binds the routes to the appropriate handler in the
  */
-const mapRoutes = (app) => {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
+const router = express.Router()
 
-export default mapRoutes;
-module.exports = mapRoutes;
+router.get('/',AppController.getHomepage)
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+export default router;
