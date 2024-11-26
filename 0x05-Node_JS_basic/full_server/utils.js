e8af86337d04b0ee8e83781dd8f886d6242350ba
Aiimey <132620366+Aiimeey@users.noreply.github.com>
2024-11-26 11:19:57 +0100

    Create utils.js

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,31 @@
+import fs from 'fs/promises';
+/**
+ * Reads the data of students in a CSV data file.
+ */
+export const readDatabase = async (filePath) => {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');// data.toString('utf-8')
+    const lines = data.split('\n').filter(Boolean).slice(1);
+    const result = {};
+
+    lines.forEach((line) => {
+      const [firstname,,,field] = line.split(',');
+      if (!result[field]) {
+        result[field] = [];
+      }
+      result[field].push(firstname);
+
+    });
+
+    // const res = []
+    // Object.entries(result).map(([key,value])=>{
+    //     res.push(`${key} : ${value}`)
+    // })
+    return result;
+  } catch (error) {
+    throw Error(error.stack.message);
+  }
+
+};
+export default readDatabase;
+// module.exports = readDatabase;
